66a2341ae3c56c95165b2ab67fc3c32bcce1973c
Oscar Mur <oscarsusmur@gmail.com>
2024-07-26 15:01:17 +0200

    Login signup link with css

--- AI-Maker/resources/views/Auth/login.blade.php
+++ AI-Maker/resources/views/Auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - AI-Maker</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/resources/css/login.css">
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;700&display=swap" rel="stylesheet">
 </head>
