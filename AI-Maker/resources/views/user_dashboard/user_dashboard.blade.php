f6bbe215151bc933832fea6bd3c1343386a00f73
Sofi <sahlal@nakamotoeurope.com>
2024-07-29 15:42:07 +0200

    "Removed user_dashboard.blade.css file and updated stylesheet link in user_dashboard.blade.php"

--- AI-Maker/resources/views/user_dashboard/user_dashboard.blade.php
+++ AI-Maker/resources/views/user_dashboard/user_dashboard.blade.php
@@ -19,7 +19,7 @@
     </style>
     <!-- Include Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../css/user_dashboard.blade.css">
+    <link rel="stylesheet" href="../../../public/css/user_dashboard.blade.css">
 </head>
 <body>
     <div class="container-fluid">
